c1cd387214896e8dc21547a6bde02ca23de4cc7a
Lancerokobe <141099012+Lancerokobe@users.noreply.github.com>
2024-08-19 15:11:49 +0000

    Success!

--- Task115.java
+++ Task115.java
@@ -2,12 +2,12 @@ public class Task115 {
   public static void main(String args[]) {
     String a = new String("Wow");
     String b = "Wow";
-    String c = a;
-    String d = c;
+    String c = new String("Wow"); 
+    String d = c + "!";
 
-    boolean b1 = a == b;
+    boolean b1 = !(a == b);
     boolean b2 = d.equals(b + "!");
-    boolean b3 = !c.equals(a);
+    boolean b3 = c.equals(a);
 
     if (b1 && b2 && b3) {
         System.out.println("Success!");
